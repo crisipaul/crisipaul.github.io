1df5d5c63ef7dd7fa5712975658cb00c93918dd9
crisipaul <78030455+crisipaul@users.noreply.github.com>
2021-02-04 16:36:57 +0000

    Update menu-color.js

--- menu-color.js
+++ menu-color.js
@@ -3,10 +3,10 @@
    $(window).scroll(function(){
     if ($(this).scrollTop() > 350) {
        $('.icon-bar').addClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo-light.png');
+       $('.navbar-brand img').attr('src', 'crisipaul.github.io/logo-light.png');
     } else {
        $('.icon-bar').removeClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo.png');
+       $('.navbar-brand img').attr('src', 'crisipaul.github.io/logo.png');
     }
 });
 
@@ -17,12 +17,12 @@ $(function(){
         if($(this).scrollTop() > 100) {
             $('#topbar, .cart-label').fadeOut('slow');
             $('.logo img')
-                .attr('src','img/logo.png');
+                .attr('src','crisipaul.github.io/logo.png');
         }
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
-                .attr('src','img/logo-light.png');
+                .attr('src','crisipaul.github.io/logo-light.png');
         }
     });
 });
